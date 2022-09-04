8446063dcaafca4ed740f0b55c27b8519b0aef16
Tzvi-H <16guitar@gmail.com>
2022-09-04 12:24:48 -0600

    3.5

--- phonebook-backend/controllers/persons.js
+++ phonebook-backend/controllers/persons.js
@@ -17,6 +17,14 @@ personsController.get("/:id", (req, res) => {
   }
 });
 
+personsController.post("/", (req, res) => {
+  const id = Math.floor(Math.random() * 10000);
+  const { name, number } = req.body;
+  const newPerson = { id, name, number };
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
 personsController.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
