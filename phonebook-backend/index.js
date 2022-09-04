cd6065958d300d5cb42a973e44f186349b90ce81
Tzvi-H <16guitar@gmail.com>
2022-09-04 12:10:43 -0600

    3.2

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const data = require("./data.json");
 
 const app = express();
 
@@ -10,6 +11,14 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use("/api/persons", personsController);
+
+app.get("/info", (req, res) => {
+  const peopleCount = data.length;
+  const currentDate = Date().toLocaleString();
+  res.send(`Phonebook has info for ${peopleCount} people
+  <br><br>
+  ${currentDate}`);
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
