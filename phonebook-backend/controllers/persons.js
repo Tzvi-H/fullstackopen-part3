e84fb01e767528a72013fc10657601212d587831
Tzvi-H <16guitar@gmail.com>
2022-09-04 12:28:05 -0600

    3.6

--- phonebook-backend/controllers/persons.js
+++ phonebook-backend/controllers/persons.js
@@ -20,6 +20,15 @@ personsController.get("/:id", (req, res) => {
 personsController.post("/", (req, res) => {
   const id = Math.floor(Math.random() * 10000);
   const { name, number } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "name is missing" });
+  } else if (!number) {
+    return res.status(400).json({ error: "number is missing" });
+  } else if (persons.some((person) => person.name === name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
   const newPerson = { id, name, number };
   persons = persons.concat(newPerson);
   res.json(newPerson);
