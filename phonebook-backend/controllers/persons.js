c8786ae2002c4343092d518e6ed4c09fb0bbaae7
Tzvi-H <16guitar@gmail.com>
2022-09-04 12:13:58 -0600

    3.3

--- phonebook-backend/controllers/persons.js
+++ phonebook-backend/controllers/persons.js
@@ -1,9 +1,20 @@
 const personsController = require("express").Router();
 
-const data = require("../data.json");
+const persons = require("../data.json");
 
 personsController.get("/", (req, res) => {
-  res.json(data);
+  res.json(persons);
+});
+
+personsController.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) {
+    res.status(404).end();
+  } else {
+    res.json(person);
+  }
 });
 
 module.exports = personsController;
